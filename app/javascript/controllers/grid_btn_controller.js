76257be87f3d1be303a3f6ddd3b98e4fedbb73d0
dk <denis.kolokolov@icloud.com>
2022-10-19 19:40:37 +0200

    query string with button

--- app/javascript/controllers/grid_btn_controller.js
+++ app/javascript/controllers/grid_btn_controller.js
@@ -6,23 +6,53 @@ export default class extends Controller {
     console.log("HELLO FROM GRID-BTN CONTROLLER!")
   }
 
-  change(e) {
+  change(event) {
 
-    e.preventDefault();
+    event.preventDefault();
 
     const grids = ['grid-btn2', 'grid-btn4', 'grid-btn6', 'grid-btn1'];
 
     let currentClass = this.element.classList[0];
     let currentIndex = grids.indexOf(currentClass);
 
+    // Updating the class of the grid button
     if(currentIndex < grids.length - 1) {
       this.element.classList.remove(currentClass);
       currentIndex += 1;
       this.element.classList.add(`${grids[currentIndex]}`);
+
+      // Updating the url if there is a query string or not
+      let currentURL = event.target.baseURI;
+      let queryString = window.location.search; // we can check if there is a query string
+
+      if (queryString.length === 0) {
+        window.location.href = `${currentURL}?grid=${grids[currentIndex]}`;
+        console.log(queryString);
+      } else {
+        window.location.href = `${currentURL}&grid=${grids[currentIndex]}`;
+        console.log(queryString);
+      }
+
+
     } else {
       this.element.classList.remove(currentClass);
       currentIndex = 0;
       this.element.classList.add(`${grids[currentIndex]}`);
+
+
+      // Updating the url if there is a query string or not
+      let currentURL = event.target.baseURI;
+      let queryString = window.location.search;
+
+      if (queryString.length === 0) {
+        window.location.href = `${currentURL}?grid=${grids[currentIndex]}`;
+        console.log(queryString);
+      } else {
+        window.location.href = `${currentURL}&grid=${grids[currentIndex]}`;
+        console.log(queryString);
+      }
+
+
     }
 
   }
